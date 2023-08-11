8d850ba838c3eb1e9a5bbb1a1fe092d8bb2100f4
Anna Waluśkiewicz <annabogatko06@gmail.com>
2023-08-11 13:04:55 +0200

    fixed error line 17

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import "./styles.css"
+import "./styles.css";
 
-export default function App (){
+export default function App() {
   return (
-  <>
-  <form className="new-item-form">
-<div className="form-row">
-<label htmlFor="item">New Item</label>
-<input type="text" id="item"></input>
-</div>
-<button className="btn">Add</button>
-  </form>
-  <h1 className="header">To Do List</h1>
-  <ul className="list"> 
-  <li>
- <label>
-      <input type="checkbox">Item 1</input>
-    </label>
-    <button className="btn btn-danger">Delete</button>
-  </li>
-  <li>
- <label>
-      <input type="checkbox">Item 2</input>
-    </label>
-    <button className="btn btn-danger">Delete</button>
-  </li>
-  </ul>
-  </> 
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item"></input>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To Do List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" /> Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" /> Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
   );
 }
